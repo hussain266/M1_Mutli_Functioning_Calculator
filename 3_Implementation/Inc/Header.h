bba90bb14624432ec5d6e763b84811dbf57fbced
hussain266 <94312393+hussain266@users.noreply.github.com>
2021-11-24 00:53:44 +0530

    Add files via upload

---
+++ 3_Implementation/Inc/Header.h
@@ -0,0 +1,22 @@
+#ifndef _head_H
+#define _head_H
+#include <stdio.h>
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+#define note "maze mat lo, enter the valid operation"
+void addition();
+void subtraction();
+void multiplication();
+void division();
+void modulus();
+void factorial();
+void power();
+void square();
+void cube();
+void squareroot();
+void Percentage_of_Profit_or_Loss();
+    
+#endif
