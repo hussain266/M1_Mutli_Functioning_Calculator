c61f4e6d8a49b6d84252bcce1a30b4c31d342a5e
hussain266 <94312393+hussain266@users.noreply.github.com>
2021-11-24 00:52:45 +0530

    Add files via upload

---
+++ 3_Implementation/SRC/main.c
@@ -0,0 +1,79 @@
+#include "Header.h"
+#include "stdio.h"
+
+int main(){
+    printf("\t\tWelcome to the scientific calculator!!\n\n");
+    int choice;
+    printf("\n******* Press 0 to quit the program ********\n");
+    printf("Enter 1 for Addition \n");
+    printf("Enter 2 for Subtraction \n");
+    printf("Enter 3 for Multiplication \n");
+    printf("Enter 4 for Division \n");
+    printf("Enter 5 for Modulus\n");
+    printf("Enter 6 for Power \n");
+    printf("Enter 7 for Factorial \n");
+    printf("Enter 8  for square \n");
+    printf("Enter 9  for cube \n");
+    printf("Enter 10 for squareroot\n\n");
+    printf("Enter 11 for Percentage of Profit or loss\n");
+    
+    while(1){    
+    printf("\n\nEnter the operation you want to do: ");
+    
+    scanf("%d",&choice);
+            
+    switch(choice)
+    {
+                case 1:
+                    addition();
+                    break;
+                case 2:
+                    subtraction();
+                    break;
+                case 3:
+                    multiplication();
+                    break;
+                case 4:
+                    division();
+                    break;
+                case 5:
+                    modulus();
+                    break;
+                case 6:
+                    power();
+                    break;
+                case 7:
+                    factorial();
+                    break;
+                case 8:
+                    square();
+                    break;
+                case 9:
+                    cube();
+                    break;
+                case 10:
+                    squareroot();
+                    break;
+                case 11:
+                    Percentage_of_Profit_or_Loss();
+                    break;
+                case 0:
+                    exit(0);
+                default:
+                    printf("\n********** %s ***********\n",note);
+        }
+    
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
