867eccc17b10e115880cc8ca441c0ba6844cac7a
hussain266 <94312393+hussain266@users.noreply.github.com>
2021-11-25 10:16:13 +0530

    Add files via upload

--- 3_Implementation/SRC/main.c
+++ 3_Implementation/SRC/main.c
@@ -1,6 +1,19 @@
-#include "Header.h"
-#include "stdio.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
 
+#define note "maze mat lo, enter the valid operation"
+void addition();
+void subtraction();
+void multiplication();
+void division();
+void modulus();
+void factorial();
+void power();
+void square();
+void cube();
+void squareroot();
+void Percentage_of_Profit_or_Loss();
 int main(){
     printf("\t\tWelcome to the scientific calculator!!\n\n");
     int choice;
@@ -67,13 +80,94 @@ int main(){
     return 0;
 }
 
-
-
-
-
-
-
-
-
-
-
+void addition(){
+    int a,n,i=0,sum=0;
+    printf("How many values u want to add ");
+    scanf("%d",&n);
+    while(i<n){
+        printf("Enter the value");
+        scanf("%d",&a);
+        sum+=a;
+        i++;
+    }
+    printf("The sum of Entered values is=%d\n",sum);
+    
+}
+void subtraction(){
+    printf("Enter the numbers you want to subtract: ");
+    int a,b;
+    scanf("%d%d",&a,&b);
+    printf("The subtraction of a and b is %d\n",a-b);
+}
+void multiplication(){
+    printf("Enter the numbers you want to multiply: ");
+    int a,b;
+    scanf("%d%d",&a,&b);
+    printf("The multiplication of a and b is %d\n",a*b);
+}
+void division(){
+    printf("Enter the numbers you want to divide: ");
+    int a,b;
+    scanf("%d%d",&a,&b);
+    printf("The division of a and b is %f\n",(float)a/(float)b);
+}
+void modulus(){
+    printf("Enter the numbers you want to find modulus of: ");
+    int a,b;
+    scanf("%d%d",&a,&b);
+    printf("The modulus of a and b is %d\n",a%b);
+}
+void factorial(){
+    int n,factorial;
+    printf("Enter the number you want the factorial of: ");
+    scanf("%d",&n);
+    factorial=1;
+    for(int i=1;i<=n;i++){
+        factorial=factorial*i; // factorial*=i;
+    }
+    printf("\nFactorial of %d is %d",n,factorial);
+}
+void power(){
+    double b;
+    double p;
+    printf("Enter the base and the power: ");
+    scanf("%lf%lf",&b,&p);
+    double e=pow(b,p);
+    printf("The power is %lf",e);
+}
+void square(){
+    double b;
+    printf("Enter the number you want the square of: ");
+    scanf("%lf",&b);
+    double p=pow(b,2);
+    printf("The square of %lf is %lf",b,p);
+}
+void cube(){
+    double b;
+    printf("Enter the number you want the cube of: ");
+    scanf("%lf",&b);
+    double p=pow(b,3);
+    printf("The cube of %lf is %lf",b,p);
+}
+void squareroot(){
+    double b;
+    printf("Enter the number you want the square root of : ");
+    scanf("%lf",&b);
+    double s = sqrt(b);
+    printf("The square root of %lf is %lf",b,s);
+}
+void Percentage_of_Profit_or_Loss(){
+    float b,c,Proift_Percentage,loss_Percentage;
+    printf("Enter the amount you have earned");
+    scanf("%f",&b);
+    printf("Enter the Investment u have made");
+    scanf("%f",&c);
+    if(c<b){
+        Proift_Percentage=((b-c)/c)*100;
+        printf("Your Proift_Percentage=%.2f",Proift_Percentage);
+    }
+    else{
+        loss_Percentage=((c-b)/c)*100;
+        printf("Your loss_Percentage=%.2f",loss_Percentage);
+    }
+}
